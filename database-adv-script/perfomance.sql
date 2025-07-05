f3a2b97d203ee789e81813d279620e5ec0bed2c2
PantheraNestah <pantheranestah@gmail.com>
2025-07-05 05:47:38 +0300

    adding 'WHERE' 'AND'

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -55,6 +55,37 @@ JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
 LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
 
+EXPLAIN ANALYZE
+SELECT 
+  b.booking_id,
+  b.start_date,
+  b.end_date,
+  b.total_price,
+  b.status,
+
+  u.user_id,
+  u.first_name,
+  u.last_name,
+  u.email,
+
+  p.property_id,
+  p.name AS property_name,
+  p.location,
+  p.pricepernight,
+
+  pay.payment_id,
+  pay.amount AS payment_amount,
+  pay.payment_method
+
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id
+
+WHERE u.first_name = 'Alice'
+  AND b.status = 'confirmed';
+
+
 
 SELECT 
   b.booking_id,
