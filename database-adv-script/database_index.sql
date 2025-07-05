5d95a763277f6d6e9958d1b6b295e730aa97a045
PantheraNestah <pantheranestah@gmail.com>
2025-07-05 05:44:15 +0300

    adding EXPLAIN ANALYZE to measure query performance before and after adding indexes

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -6,3 +6,15 @@ ON Booking (booking_id);
 
 CREATE INDEX idx_property_id 
 ON Property (property_id);
+
+
+EXPLAIN ANALYZE
+SELECT 
+  b.booking_id,
+  u.first_name,
+  u.last_name,
+  p.name AS property_name
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+WHERE u.first_name = 'Alice' AND u.last_name = 'Johnson';
