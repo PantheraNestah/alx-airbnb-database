71f0674d56d84ebd83e66e66a550f973b9ae9622
PantheraNestah <pantheranestah@gmail.com>
2025-07-05 05:31:00 +0300

    adding order by to left join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,7 +4,8 @@ INNER JOIN Booking ON User.user_id = Booking.user_id;
 
 SELECT Property.property_id, Property.name, Review.rating, Review.comment
 FROM Property
-LEFT JOIN Review ON Property.property_id = Review.property_id;
+LEFT JOIN Review ON Property.property_id = Review.property_id
+ORDER BY Property.property_id;
 
 SELECT User.user_id, User.first_name, User.last_name, Booking.booking_id, Booking.property_id, Booking.start_date, Booking.end_date
 FROM User
