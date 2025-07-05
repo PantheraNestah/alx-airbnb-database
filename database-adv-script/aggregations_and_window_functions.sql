fafa626994352797270e6c50d92cc80008dee3c4
PantheraNestah <pantheranestah@gmail.com>
2025-07-05 05:36:19 +0300

    adding RANK to rank properties based on the total number of bookings they have received

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -3,3 +3,14 @@ SELECT
     COUNT(booking_id) AS total_bookings
 FROM Booking
 GROUP BY user_id;
+
+
+SELECT 
+  p.property_id,
+  p.name AS property_name,
+  COUNT(b.booking_id) AS total_bookings,
+  RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM properties p
+LEFT JOIN bookings b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name
+ORDER BY booking_rank;
